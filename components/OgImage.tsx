92f436ecb1f037bdb51f55629d966b8dc4d13015
mdraihan34988 <swammia908@gmail.com>
2022-10-22 15:06:40 +0600

    add

---
+++ components/OgImage.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from "next/image";
+
+ function OgImage({ src, alt, darkSrc }) {
+
+  return (
+    <div className="relative -mt-[35%] sm:-mt-0 md:-ml-[35%] w-full sm:w-1/2 md:w-8/12 shrink-0 rounded-xl overflow-hidden shadow-2xl before:absolute before:inset-0 dark:before:bg-black/20 before:z-10">
+      <Image
+        title={alt}
+        alt={alt}
+        src={src}
+        width={1200}
+        height={630}
+        layout="responsive"
+        placeholder="blur"
+        objectFit="cover"
+        blurDataURL={src}
+        quality={50}
+        className="lg:group-hover:scale-110 transition-all duration-300 backdrop-blur-xl"
+      />
+    </div>
+  );
+}
+
+export default OgImage;
